c9ee9a1002968f3c79a0c1d3c08291f6f765e554
Emil Ejebring <dev@ejebring.com>
2022-10-25 09:04:30 +0200

    Initial Commit

---
+++ Task-List/Task_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ListApp.swift
+//  Task-List
+//
+//  Created by Emil Ejebring on 2022-10-25.
+//
+
+import SwiftUI
+
+@main
+struct Task_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
